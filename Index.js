0717ee6cab9788ff172bd9c9bd7b1c111dbfb64d
Abi0 <Abi.@abi.com>
2024-09-17 01:23:50 -0300

    add new e-commerce

---
+++ Index.js
@@ -0,0 +1,22 @@
+<script src="js/bootstrap.min.js></script>
+
+document.querySelector("h1").innerText = "Productos"
+
+let arrProds = [];
+
+for (let i = 1; i <= 10; i++) {
+    CardsArray push(
+    <div class="card">
+        <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="Producto ${i}"></img>
+        <div class="card-body">
+            <h5 class="card-tittle">Car $(i)</h5>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt elementum aliquam. Sed a justo dui. Nunc vulputate luctus cursus. Nulla facilisi. Ut vitae mollis augue.</p>
+            <p class="card-price">$11111</p>
+            <buttom type="buttom" class="btn-outline">Buy Now</buttom></p>
+        </div>
+    </div> );
+
+    CardsArray.push(card);
+}
+
+Selection.innerHTML = array.join().replaceAll(",","");
