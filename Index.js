1a1347c2a1dd013b7243bc915f3134ca7350784a
Abrils <abro@gmail.com>
2024-09-18 23:20:07 -0300

    modificacion con css

--- Index.js
+++ Index.js
@@ -2,16 +2,16 @@ document.querySelector("h1").innerText = "Productos";
 
 let CardsArray = [];
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 1; i <= 9; i++) {
     CardsArray.push(
     `
     <div class="card"> 
-        <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" alt="Producto ${i}"></img>
+        <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img" alt="Producto ${i}"></img>
         <div class="card-body">
-            <h5 class="card-tittle">Car $(i)</h5>
+            <h5 class="card-title">Car $(i)</h5>
             <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur tincidunt elementum aliquam. Sed a justo dui. Nunc vulputate luctus cursus. Nulla facilisi. Ut vitae mollis augue.</p>
-            <p class="card-price">$11111</p>
-            <buttom type="buttom" class="btn-outline">Buy Now</buttom></p>
+            <p class="card-price">$11.000.000</p>
+            <button type="button" class="btn-outline">But Now</button>
         </div>
     </div> `);
 }
